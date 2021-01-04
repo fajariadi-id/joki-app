d85c86495f687d56c22bcf97b523235c4a5a0418
fajariadi-id <fajariadi.id@gmail.com>
2021-01-04 17:18:07 +0700

    initial commit

---
+++ build/assets/script/data/dataArrays.js
@@ -0,0 +1,95 @@
+// BENEFITS DATA
+const benefits = [
+  {
+    img: "./assets/img/deadline.svg",
+    title: "Deadline Kapan Saja",
+  },
+  {
+    img: "./assets/img/kelas.svg",
+    title: "Semua Kelas Pelajar/Mahasiswa/Umum",
+  },
+  {
+    img: "./assets/img/harga.svg",
+    title: "Harga Terjangkau",
+  },
+];
+
+// CARD PRICELIST DATA
+const pricelist = [
+  {
+    title: "SD/SMP/SMA",
+    price: "10K",
+    features1: "Deadline H-7",
+    features2: "1 kali revisi",
+  },
+  {
+    title: "Semua Kalangan",
+    price: "50K",
+    features1: "Deadline H-3",
+    features2: "2 kali revisi",
+  },
+  {
+    title: "Kuliah/Umum",
+    price: "25K",
+    features1: "Deadline H-7",
+    features2: "1 kali revisi",
+  },
+];
+
+// CAROUSEL DATA
+const testimonials = [
+  {
+    img: "https://randomuser.me/api/portraits/women/7.jpg",
+    title: "Rasanya pengen meninggoy!",
+    body:
+      "Udah panik banget H-3 tugas makalah harus dikumpul, sedangkan laptop aku lagi rusak. Untung temenku rekomendasiin jasa joki ini. Alhamdulillah jadi gak harus ngulang mata kuliah😭",
+    name: "Indah",
+    job: "Mahasiswi",
+  },
+  {
+    img: "https://randomuser.me/api/portraits/men/7.jpg",
+    title: "Nyantai banget!",
+    body:
+      "Kakak-kakaknya beneran santai dan sabar banget. Kayanya bakal rekomendasiin temen-temen deh buat mampir kesini.",
+    name: "Kale",
+    job: "Pelajar",
+  },
+  {
+    img: "https://randomuser.me/api/portraits/women/20.jpg",
+    title: "Nyesel!",
+    body:
+      "Nyesel banget baru tau ada jasa joki kaya gini sekarang😭 Worth it banget! Kalo ada member, mau deh bikin member!",
+    name: "Dinda",
+    job: "Karyawan",
+  },
+];
+
+// CARD TEAM DATA
+const teams = [
+  {
+    img: "https://randomuser.me/api/portraits/men/3.jpg",
+    name: "Dhito",
+    majoring: "Arsitektur",
+    university: "Institut Teknologi Bandung",
+    desc:
+      "Halo, saya yang akan mengerjakan tugas mu untuk bidang ilmu menggambar dan tata letak bangunan.",
+  },
+  {
+    img: "https://randomuser.me/api/portraits/men/77.jpg",
+    name: "Pramono",
+    majoring: "Sistem Komputer",
+    university: "Universitas Gunadarma",
+    desc:
+      "Halo, saya yang akan mengerjakan tugas mu untuk bidang matematika dan ilmu komputer.",
+  },
+  {
+    img: "https://randomuser.me/api/portraits/women/17.jpg",
+    name: "Nindya",
+    majoring: "Sastra Inggris",
+    university: "Universitas Padjadjaran",
+    desc:
+      "Halo, saya yang akan mengerjakan tugas mu untuk bidang bahasa inggris dan ilmu komunikasi.",
+  },
+];
+
+export { benefits, pricelist, testimonials, teams };
